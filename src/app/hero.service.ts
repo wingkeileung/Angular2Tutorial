c49f9559d1f37ec56a6dffa18c5a44a3b596d1ff
Wing Kei Leung <winky.leung@gmail.com>
2017-12-08 16:52:34 -0800

    fixed one syntax error on hero.service.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -18,7 +18,7 @@ export class HeroService {
 
   constructor(
     private http: HttpClient,
-    private messageService: MessageService) { },
+    private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
